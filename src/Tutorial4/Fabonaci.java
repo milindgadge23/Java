2bccb0eff48da6d7458d8b88e4459a89156149f6
swati <swatikasture93@gmail.com>
2024-07-20 16:34:45 +0530

    one program added

---
+++ src/Tutorial4/Fabonaci.java
@@ -0,0 +1,18 @@
+package Tutorial4;
+
+public class Fabonaci {
+
+	public static void main(String[] args) {
+		int a=0;int b=1;int c;
+		for(int i=1;i<=10;i++)
+		{
+			
+			c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+		}
+
+	}
+
+}
