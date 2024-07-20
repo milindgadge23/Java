b9689e47dfa5ee7198a09bc60f4e2e6b5d5bb0df
unknown <shambhosale1212@gmail.com>
2024-07-20 16:54:44 +0530

    Anagram program is created

---
+++ src/Tutorial/Anagram.java
@@ -0,0 +1,37 @@
+package Tutorial;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter String 1");
+		
+		String s=sc.nextLine();
+		 
+		System.out.println("Enter String 2");
+		
+		String v=sc.nextLine();
+		
+		char x[]=s.toCharArray();
+		Arrays.sort(x);
+		
+		char [] z=v.toCharArray();
+		Arrays.sort(z);
+		
+		
+		if (Arrays.equals(x, z))
+		{
+			System.out.println("String is Anagram");
+		}
+		else
+		{
+			System.out.println("String is not Anagram");
+		}
+	}
+
+}
